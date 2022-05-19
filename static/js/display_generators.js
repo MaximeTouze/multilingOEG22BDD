769ea18f2d12a0d5303a8b51cae6028f557c7346
Julien Aubert <julienaubeb@gmail.com>
2022-05-19 16:05:12 +0200

    feat: Better likes

--- static/js/display_generators.js
+++ static/js/display_generators.js
@@ -16,49 +16,31 @@ const GenerateExposedImg = function (name, style="weight:100%; height:100%", onc
   return GenerateImg("../static/exposed/" + name, name, style=style, onclick=onclick, id);
 }
 
-const GenerateSentence = function (sentence, nb_sentence, pict_id=null, has_pict=true, nb_likes=null) {
-  let res =  '<div id="' + getSentenceId(nb_sentence) + '"> <p> ' + sentence + ' <\p> ';
 
-  if (nb_likes != null) {
-    res += GenerateSentenceNbLikes(nb_sentence, nb_likes);
-  }
-
-  if ( has_pict) {
-    res += GenerateSentenceImg(nb_sentence, pict_id, has_pict);
-  }
-  //console.log(res);
-  return res + ' <\div><div class="sentence-separator">' + SENTENCE_SEPARATOR + ' <\div>';
-
-}
-
-const GenerateSentenceImg = function (nb_sentence, pict_id, has_pict=true) {
-  if (has_pict) {
-    const onclickFunction = 'LikeSentence(' + nb_sentence + ');';
-    const imgStyle = "max-width:2% !important; max-height:2% !important;";
-    const imgId = getImageId(nb_sentence);
-    let img = GenerateExposedImg('grey_thumbs_up.' + IMG_TYPE , style=imgStyle, onclick=onclickFunction, imgId);
-    return '<p id=' + pict_id + '>' + img + ' <\p>';
-  }
-  return "";
-}
-
-const GenerateSentenceNbLikes = function (nb_sentence, nb_likes) {
-  return '<div class="like-details">' + nb_likes + " <\div>";
+const ChangeSentenceDislikeButtonToLike = function (nb_sentence) {
+  const onclickFunction = 'LikeSentence(' + nb_sentence + ');';
+  document.getElementById('like_'+nb_sentence).classList.remove('liked');
+  document.getElementById(getSentenceId(nb_sentence)).setAttribute('onclick',onclickFunction)
 }
 
 const ChangeSentenceLikeButtonToUnlike = function (nb_sentence) {
-  const imgId = getImageId(nb_sentence);
-  const pict_id = getSentenceImgId(nb_sentence);
   const onclickFunction = 'UnlikeSentence(' + nb_sentence + ');';
-  const imgStyle = "max-width:2% !important; max-height:2% !important;";
-  document.getElementById(pict_id).innerHTML = GenerateExposedImg('green_thumbs_up.' + IMG_TYPE , style=imgStyle, onclick=onclickFunction, imgId);
+  document.getElementById('like_'+nb_sentence).classList.add('liked');
+  document.getElementById(getSentenceId(nb_sentence)).setAttribute('onclick',onclickFunction)
 }
 
-
-const ChangeSentenceDislikeButtonToLike = function (nb_sentence) {
-  const imgId = getImageId(nb_sentence);
-  const pict_id = getSentenceImgId(nb_sentence);
+const GenerateSentence = function (sentence, nb_sentence, pict_id=null, has_pict=true, nb_likes=null) {
   const onclickFunction = 'LikeSentence(' + nb_sentence + ');';
-  const imgStyle = "max-width:2% !important; max-height:2% !important;";
-  document.getElementById(pict_id).innerHTML = GenerateExposedImg('grey_thumbs_up.' + IMG_TYPE , style=imgStyle, onclick=onclickFunction, imgId);
+  let res = '<div class="sentence" id="' + getSentenceId(nb_sentence) + '" onclick='+onclickFunction+'>';
+  res += '<p>'+ sentence + '<\p>';
+  
+  if(!nb_likes){
+    nb_likes= "";
+  }
+  if (has_pict) {
+    res += '<div id=like_' + nb_sentence + ' class="heart" style="">'+nb_likes+'</div>';
+  }
+  res += '</div>';
+  console.log(res);
+  return res;
 }
