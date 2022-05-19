4cf9245d5c450e613784af099b73ed5f2b735f7c
MaximeTouze <titouf-72@hotmail.fr>
2022-05-19 11:20:39 +0200

    /view

--- static/js/display_updator/display_updator_custommer.js
+++ static/js/display_updator/display_updator_custommer.js
@@ -18,7 +18,7 @@ function updateSentence () {
       const keys = Object.keys(sentences_);
 
       for (var i = 0; i < keys.length; i++) {
-        sentence_rank = key[i];
+        sentence_rank = keys[i];
         if (!document.getElementById(getSentenceId(sentence_rank))) {
           current_nb_sentence ++;
           elt.innerHTML += GenerateSentence(sentences_[sentence_rank],  sentence_rank, getSentenceImgId(sentence_rank, true));
